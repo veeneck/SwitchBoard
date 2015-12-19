99350afc47fd17fa53051bf1c8ebb5e1960f381f
Ryan Campbell <veeneck@gmail.com>
2015-12-19 16:01:35 -0500

    Initial Commit

---
+++ SwitchBoard/SwitchBoard.h
@@ -0,0 +1,19 @@
+//
+//  SwitchBoard.h
+//  SwitchBoard
+//
+//  Created by Ryan Campbell on 12/19/15.
+//  Copyright © 2015 Phalanx Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwitchBoard.
+FOUNDATION_EXPORT double SwitchBoardVersionNumber;
+
+//! Project version string for SwitchBoard.
+FOUNDATION_EXPORT const unsigned char SwitchBoardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwitchBoard/PublicHeader.h>
+
+
